aa450a80ab75998956c99a9e140bf0074fb63982
codingronin <codingronin@bitcoin2.network>
2019-02-16 04:00:31 -0600

    Project specific code changes

--- php_api_src/config.php
+++ php_api_src/config.php
@@ -3,5 +3,5 @@
 $testnet = false;
 $cacheLocation = __DIR__.'/'.($testnet ? 'cache-testnet' : 'cache');
 $daemonAddress = '127.0.0.1';
-$rpcPort = $testnet ? 12001 : 30081;
-$coinSymbol = 'lmo';
+$rpcPort = $testnet ? 19035 : 19935;
+$coinSymbol = 'b2n';
